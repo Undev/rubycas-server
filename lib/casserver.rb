a062a9a3f05737d2ccc5b7ed0bb7d9081862b3f0
Nick Recobra <oruen@undev.ru>
2011-01-26 17:53:21 +0300

    Fixing #serviceValidation identation. The right way.

--- lib/casserver.rb
+++ lib/casserver.rb
@@ -8,18 +8,3 @@ require 'logger'
 $LOG = Logger.new(STDOUT)
 
 require 'casserver/server'
-module Tilt
-  class BuilderTemplate < Tilt::Template
-    def evaluate(scope, locals, &block)
-      # Changing indent to 0 to adjust generated xml to play well with OmniAuth CAS strategy. That's tested with Tilt 1.1 only.
-      xml = ::Builder::XmlMarkup.new(:indent => 0)
-      if data.respond_to?(:to_str)
-        locals[:xml] = xml
-        super(scope, locals, &block)
-      elsif data.kind_of?(Proc)
-        data.call(xml)
-      end
-      xml.target!
-    end
-  end
-end
